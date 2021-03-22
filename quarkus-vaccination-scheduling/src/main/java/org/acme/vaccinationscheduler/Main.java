4fd000c4c87ac05eb2176ee875a464abc2456d00
Lukáš Petrovický <lukas@petrovicky.net>
2021-03-22 11:24:50 +0100

    FUrther improve the benchmark

--- quarkus-vaccination-scheduling/src/main/java/org/acme/vaccinationscheduler/Main.java
+++ quarkus-vaccination-scheduling/src/main/java/org/acme/vaccinationscheduler/Main.java
@@ -16,22 +16,34 @@
 
 package org.acme.vaccinationscheduler;
 
-import java.util.Collections;
+import java.time.Duration;
+import java.time.Instant;
 import org.acme.vaccinationscheduler.bootstrap.DemoDataGenerator;
-import org.acme.vaccinationscheduler.domain.Person;
 import org.acme.vaccinationscheduler.domain.VaccinationSchedule;
+import org.optaplanner.core.api.score.buildin.hardmediumsoftlong.HardMediumSoftLongScore;
 import org.optaplanner.core.api.score.stream.ConstraintStreamImplType;
 import org.optaplanner.core.api.solver.Solver;
 import org.optaplanner.core.api.solver.SolverFactory;
+import org.optaplanner.core.config.phase.PhaseConfig;
 import org.optaplanner.core.config.solver.SolverConfig;
+import org.optaplanner.core.config.solver.termination.TerminationConfig;
+
+import static org.optaplanner.core.api.score.stream.ConstraintStreamImplType.DROOLS;
 
 public class Main {
 
-    private static Solver<VaccinationSchedule> getSolver(ConstraintStreamImplType constraintStreamImplType) {
+    private static Solver<VaccinationSchedule> getSolver(ConstraintStreamImplType constraintStreamImplType,
+                                                         HardMediumSoftLongScore expectedScore) {
         SolverConfig solverConfig = SolverConfig.createFromXmlResource("org/acme/vaccinationscheduler/solverConfig.xml");
-        solverConfig.setSolutionClass(VaccinationSchedule.class);
-        solverConfig.setEntityClassList(Collections.singletonList(Person.class));
         solverConfig.getScoreDirectorFactoryConfig().setConstraintStreamImplType(constraintStreamImplType);
+        TerminationConfig terminationConfig = new TerminationConfig();
+        if (constraintStreamImplType == DROOLS) { // If DROOLS, run until we reach the expected score.
+            terminationConfig.setBestScoreLimit(expectedScore.toString());
+        } else { // If BAVET, run local search for 10 minutes after construction heuristics terminates.
+            terminationConfig.setMinutesSpentLimit(10L);
+        }
+        PhaseConfig<?> localSeachConfig = solverConfig.getPhaseConfigList().get(1);
+        localSeachConfig.setTerminationConfig(terminationConfig);
         return SolverFactory.<VaccinationSchedule>create(solverConfig)
                 .buildSolver();
     }
@@ -47,12 +59,24 @@ public class Main {
         //                            vvvvv Change to 20, 100 for medium data set.
         //                            vvvvv Change to 40, 1200 for production data set.
         VaccinationSchedule problem = generateData(40, 1200);
-        // First run Bavet.
-        Solver<VaccinationSchedule> solver = getSolver(ConstraintStreamImplType.BAVET);
-        solver.solve(problem);
-        // Then run Drools in the same JVM, to get comparable numbers.
-        Solver<VaccinationSchedule> solver2 = getSolver(ConstraintStreamImplType.DROOLS);
+
+        // First run Bavet and store the score it reaches within the preconfigured interval.
+        Solver<VaccinationSchedule> solver = getSolver(ConstraintStreamImplType.BAVET, null);
+        Instant bavetStart = Instant.now();
+        VaccinationSchedule bestSolution = solver.solve(problem);
+        Duration bavetDuration = Duration.between(Instant.now(), bavetStart).abs();
+
+        // Then run Drools in the same JVM to get comparable performance, and ask it to reach the same score as BAVET.
+        Solver<VaccinationSchedule> solver2 = getSolver(DROOLS, bestSolution.getScore());
+        Instant droolsStart = Instant.now();
         solver2.solve(problem);
+        Duration droolsDuration = Duration.between(Instant.now(), droolsStart).abs();
+
+        // Now tabulate the results so that they do not get lost in possibly a lengthy log.
+        System.out.println("==========");
+        System.out.println("Target score: " + bestSolution.getScore());
+        System.out.println("  BAVET took: " + bavetDuration);
+        System.out.println(" DROOLS took: " + droolsDuration);
     }
 
 }
